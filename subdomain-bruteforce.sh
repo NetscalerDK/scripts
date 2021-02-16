dfde24f179fe27df1aeb3efb0a30eb858dfbf5ce
Mads Behrendt Petersen <mbp@netscaler.dk>
2021-02-16 12:34:19 +0100

    Added subdomain bruteforcing script

---
+++ subdomain-bruteforce.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+#takes input from a file, where the format is a word pr line, and returns only the subdomain that returns OK
+domain=$1
+xargs -P10 -n1 -I{} bash -c "
+  if host \"{}.$domain\" &> /dev/null; then
+    echo \"{}.$domain\"
+  fi
+"
